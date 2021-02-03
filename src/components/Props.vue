87d12844ef41c643d20ab6af0639c42783ee5f67
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-02-03 18:03:46 +0100

    Add Props.vue

---
+++ src/components/Props.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-if="show">
+        <img alt="Volvo Amazon - polisbil" src="../assets/polisbil.png"/>
+    </div>
+    <div v-else>
+        <input @click="wtf()" type="button" value="wtf?"/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            show: this.init
+        }
+    },
+    name: 'Props',
+    methods: {
+        wtf() {
+            this.show = true
+        }
+    },
+    props: ['init']
+}
+
+</script>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,16 +1,46 @@
 <template>
   <div id="main">
     <h1>Contact us</h1>
-    <p>We are here to help. Nah&hellip; j/k. Lol.</p>
+  <div v-if="userName">
+    <p>Hello, {{ userName }}. We're here to help. Nah&hellip; j/k. Lol.</p>
     <h2>Search our FAQ</h2>
     <dl>
       <dt>Q:</dt>
       <dd>My problem isn't listed here.</dd>
       <dt>A:</dt>
-      <dd>Naw&hellip; that's too bad. Try contacting us.</dd>
+      <dd>Naw&hellip; that's too bad. Try contacting us. Lol. You can't.</dd>
     </dl>
+    <Props :init="false"></Props>
+  </div>
+  <div v-else>
+    <p><input placeholder="Your name here" v-model="name"/>&nbsp;<input @click="onClick()" type="button" value="Ok"/></p>
+    </div>
   </div>
 </template>
+
+<script>
+
+import Props from '../components/Props.vue'
+
+export default {
+  components: {
+    Props
+  },
+  data() {
+    return {
+      userName: null,
+      name: null
+    }
+  },
+  methods: {
+    onClick() {
+      this.userName = this.name
+    }
+  },
+  name: 'Contact'
+}
+
+</script>
 
 <style scoped>
 
@@ -25,6 +55,11 @@
 
 dt {
   font-weight: bold;
+}
+
+input {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-size: 12px;
 }
 
 </style>
