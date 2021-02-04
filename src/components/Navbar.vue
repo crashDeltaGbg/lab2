4ff1d693c115cb724d26208c5aa03948367dd4c5
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-02-04 20:14:33 +0100

    Modify bootstrap-vue navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,10 @@
 <template>
     <div id="nav">
         <b-navbar>
-        <b-nav-item to="/">Home</b-nav-item>&nbsp;|&nbsp;
-        <b-nav-item to="/contact">Contact</b-nav-item>
+            <b-navbar-nav>
+                <b-nav-item to="/">Home</b-nav-item>
+                <b-nav-item to="/contact">Contact</b-nav-item>
+            </b-navbar-nav>
         </b-navbar>
     </div>
 </template>
