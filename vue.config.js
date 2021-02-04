735e0778053a07eaf5b63e67886c8e00588669a3
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-02-04 18:34:54 +0100

    Add icons

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    pwa: {
+        manifestOptions: {
+            background_color: '#7F8799',
+            short_name: 'Polisen'
+        },
+        name: 'Polisen i Göteborg',
+        themeColor: '#4370CC'
+    }
+}
