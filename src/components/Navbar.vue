4958ed5bffc7b62c61ad9bcbdbdc21736618c067
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-02-04 19:58:25 +0100

    Add bootstrap-vue component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="nav">
-        <router-link to="/">Home</router-link>&nbsp;|&nbsp;
-        <router-link to="/contact">Contact</router-link>
+        <b-navbar>
+        <b-nav-item to="/">Home</b-nav-item>&nbsp;|&nbsp;
+        <b-nav-item to="/contact">Contact</b-nav-item>
+        </b-navbar>
     </div>
 </template>
 
