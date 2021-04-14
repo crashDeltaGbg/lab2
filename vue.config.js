ed9dfcc1179ff5d48a8434c9ae35f03bfa16bee1
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-04-14 16:31:36 +0200

    set publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-    pwa: {
-        manifestOptions: {
-            background_color: '#7F8799',
-            short_name: 'Polisen'
-        },
-        name: 'Polisen i Göteborg',
-        themeColor: '#4370CC',
-        workboxOptions: {
-            runtimeCaching: [
-                {
-                    handler: 'NetworkFirst',
-                    options: {
-                        networkTimeoutSeconds: 5
-                    },
-                    urlPattern: 'https://polisen.se/api/events?locationname=Göteborg'
-                }
-            ]
-        }
-    }
-}
+	pwa: {
+		manifestOptions: {
+			background_color: "#7F8799",
+			short_name: "Polisen",
+		},
+		name: "Polisen i Göteborg",
+		themeColor: "#4370CC",
+		workboxOptions: {
+			runtimeCaching: [
+				{
+					handler: "NetworkFirst",
+					options: {
+						networkTimeoutSeconds: 5,
+					},
+					urlPattern: "https://polisen.se/api/events?locationname=Göteborg",
+				},
+			],
+		},
+	},
+	publicPath: "/vue-cli/",
+};
